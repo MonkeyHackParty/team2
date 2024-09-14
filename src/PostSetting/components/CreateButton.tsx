5daa0ef6e723e9ea1833f72d5fb176155d83902b
kugaichiro <is0722hv@ed.ritsumei.ac.jp>
2024-09-14 20:15:14 +0900

    投稿の設定フォームのリファクタリング(ファイル分類)

---
+++ src/PostSetting/components/CreateButton.tsx
@@ -0,0 +1,30 @@
+// /components/CreateButton.tsx
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+interface CreateButtonProps {
+  onPress: () => void;
+}
+
+const CreateButton: React.FC<CreateButtonProps> = ({ onPress }) => {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Text style={styles.buttonText}>Make Board</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#000',
+    padding: 15,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+  },
+});
+
+export default CreateButton;
